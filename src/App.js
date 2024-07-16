b607819c6d7dea1f52c226f454d86bd77ab9e834
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-07-16 20:57:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      write your logic here
+    </div>
+  );
+};
+
+const Nav = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default App;
